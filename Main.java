13f0753cb978f7caee2a275785647b9659c4254c
rio <riovectored@gmail.com>
2023-03-27 21:09:49 +0700

    program java main 04

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
 //menambahkan env var path dg jdk 20 bin untuk bisa menggunakan cmd
+//javac Main.java
+//java Main
 
 public class Main{
     public static void main(String[]arg){
-        System.out.println("hello world");
+        System.out.println("hello Delisha");
     }
 }
+
+/*
+
+
+setiap membuat file.java harus ada
+ public class Main{
+    program
+}
+
+class Main adalah identifier, nama depan harus besar
+class Main harus sama dengan .java
+*/

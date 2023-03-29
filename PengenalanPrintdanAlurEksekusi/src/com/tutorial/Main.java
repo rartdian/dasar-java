a747dbc9beb24ff553a0c1fd6d2ca8a1117f5b1f
rio <riovectored@gmail.com>
2023-03-29 15:12:30 +0700

    print dan alur

---
+++ PengenalanPrintdanAlurEksekusi/src/com/tutorial/Main.java
@@ -0,0 +1,21 @@
+/*
+ * membuat new project java
+ * membuat package tutorial
+ * membuat class di package dengan nama Main
+ * */
+package com.tutorial;
+
+public class Main {
+public static void main(final String[] args) {
+	/*contoh method*/System.out.println("hello world delisha, teman teman");
+	System.out.println("hai juga");
+	System.out.print("coba pakai dengan print A");
+	System.out.print("coba pakai dengan print B");
+	System.out.println("coba dengan println");
+	System.out.println("enter println");
+	/*eksekusi dari atas ke bawah*/
+	System.out.println("\n New Line enter");
+	/*print format**/System.out.printf("saras %d", 008);
+
+}
+}

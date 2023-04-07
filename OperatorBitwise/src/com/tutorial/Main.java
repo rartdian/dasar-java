33d08c0af3699849e7229856d150c5989cc629ac
rio <riovectored@gmail.com>
2023-04-07 19:16:45 +0700

    shift left

--- OperatorBitwise/src/com/tutorial/Main.java
+++ OperatorBitwise/src/com/tutorial/Main.java
@@ -19,7 +19,13 @@ public class Main {
 	byte b, c;
 	String a_bits, b_bits, c_bits;
 	
+	System.out.println("-------> ''''Shift Left (<<)'''' <-------");
+	a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+	System.out.printf("%s = %d \n", a_bits, a);
 	
+	b = (byte)(a << 2);
+	b_bits = String.format("%8s", Integer.toBinaryString(b)).replace(' ','0');
+	System.out.printf("%s = %d \n", b_bits, b);
 	}
 
 }

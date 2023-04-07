db4553ac94cb7c39b442836a71996cb96305b394
rio <riovectored@gmail.com>
2023-04-07 19:27:17 +0700

    menambahkan catatan String.format untuk menampilkan dalam bit

--- OperatorBitwise/src/com/tutorial/Main.java
+++ OperatorBitwise/src/com/tutorial/Main.java
@@ -11,7 +11,10 @@ public class Main {
 		
 	byte x = 1;
 	String x_bits;
-							//%8s menampilkan 8 string byte
+			/*
+			 * String.format itu sama saja dengan System.printf namun, 
+			 * String.format itu ditampilkan dalam bit%8s menampilkan 8 string bit
+			 * */
 	x_bits = String.format("%8s", Integer.toBinaryString(x));
 	System.out.printf("nilai bit %s = nilai desimal %d \n", x_bits, x);
 	

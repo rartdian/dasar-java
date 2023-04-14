5b5fd4ffde0a1c64cef84fb047efd2855b1dd1a2
rio <riovectored@gmail.com>
2023-04-14 14:24:39 +0700

    menambahkan statement if

---
+++ ifstatement/src/com/tutorial/Main.java
@@ -0,0 +1,18 @@
+package com.tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 5;
+		
+		System.out.println("nilai a = "+a);
+		if (a == 10) {
+			System.out.println("ini adalah jalur TRUE");
+			
+		} else {
+			System.out.println("ini adalah jalur FALSE");
+		}
+		System.out.println("selesai");
+	}
+
+}

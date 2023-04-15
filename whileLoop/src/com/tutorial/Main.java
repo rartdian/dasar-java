d5fdabe0c0af4dad0cd5ac31641400deb6e7995c
rio <riovectored@gmail.com>
2023-04-15 15:39:06 +0700

    while loop

---
+++ whileLoop/src/com/tutorial/Main.java
@@ -0,0 +1,29 @@
+package com.tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * while (kondisi){
+		 * 	aksi
+		 * }
+		 * */
+		int a = 0;
+		boolean kondisi = true;
+		
+		System.out.println("awal program");
+		while (kondisi) {
+			System.out.println("while looping ke= "+a);
+		
+			if (a == 10) {
+				kondisi = false;
+			}
+			
+			++a;
+		}
+		
+		System.out.println("program selesai.");
+
+	}
+
+}

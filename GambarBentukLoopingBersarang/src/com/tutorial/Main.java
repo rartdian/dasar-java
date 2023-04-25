4c56e949650f3321a81d1c8c33226fe551bb993d
rio <riovectored@gmail.com>
2023-04-25 07:22:30 +0700

    looping bersarang

--- GambarBentukLoopingBersarang/src/com/tutorial/Main.java
+++ GambarBentukLoopingBersarang/src/com/tutorial/Main.java
@@ -1,5 +1,7 @@
 package com.tutorial;
 
+import java.util.Iterator;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -35,6 +37,21 @@ public class Main {
 			for(int j=0;j<5;j++) {
 				System.out.print("* ");
 				if(i+j == 4 ) {
+					break;
+				}
+				
+			}
+			System.out.print("\n");
+		}
+		
+		System.out.println("\n");
+		
+		for (int i = 0; i <=10;i++) {
+			System.out.print(i);
+			for(int j= 0; j<=10; j++) {
+				
+				System.out.print(j);	
+				if(i+j == 8) {
 					break;
 				}
 				

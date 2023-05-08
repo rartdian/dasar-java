d4f4b38cab9bfcdd576564c11f4fde64dde8f60a
rio <riovectored@gmail.com>
2023-05-08 07:22:12 +0700

    membuat fungsi/method cetakArray2D

--- ArrayMultidimensi/src/com/tutorial/Main.java
+++ ArrayMultidimensi/src/com/tutorial/Main.java
@@ -12,6 +12,20 @@ int[][] array_2D = {
 		{1,2},{3,4}
 };
 
+
+
+	}
+	
+	public static void cetakArray2D(int[][] dataArray) {
+		System.out.print("\n");
+		for(int[] baris: dataArray) {
+			System.out.print("{");
+			for(int angka : baris) {
+				System.out.print(angka + ",");
+			}
+			System.out.print("}\n");
+		}
+		System.out.print("}\n");
 	}
 
 }

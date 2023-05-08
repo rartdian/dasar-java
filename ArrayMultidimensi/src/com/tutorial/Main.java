133f3140188269b1357deec046c5d6ce19b0b1b3
rio <riovectored@gmail.com>
2023-05-08 08:23:10 +0700

    menambahkan looping foreach cetak array 2d

--- ArrayMultidimensi/src/com/tutorial/Main.java
+++ ArrayMultidimensi/src/com/tutorial/Main.java
@@ -15,7 +15,7 @@ int[][] array_2D = {
 cetakArray2D(array_2D);
 
 // cara membuat array 2 dimensi dengan deklarasi
-// int [baris][kolom]
+// 					int [baris][kolom]
 int[][] arrayAngka = new int[5][4];
 cetakArray2D(arrayAngka);
 
@@ -30,6 +30,16 @@ for(int i = 0; i< array_2D.length; i++) {
 	System.out.print("]\n");
 }
 
+System.out.println("\n");
+
+//looping array2d dengan foreach
+for(int[]baris:arrayAngka) {
+	System.out.print("[");
+	for(int angka : baris) {
+		System.out.print(angka + ",");
+	}
+	System.out.print("]\n");
+}
 
 	}
 	

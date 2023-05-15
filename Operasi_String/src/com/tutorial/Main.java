4405dc1479ae6da0486d62d85bf141b29c6c9f75
rio <riovectored@gmail.com>
2023-05-15 14:04:11 +0700

    menambahkan project operasi string

---
+++ Operasi_String/src/com/tutorial/Main.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+import java.util.Scanner;
+import java.lang.String;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

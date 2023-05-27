a8be5d513090e263f22fe729dbda3cf5412644f5
rio <riovectored@gmail.com>
2023-05-27 17:57:44 +0700

    menggabungkan dua Exception

--- ExceptionTryCatchdanFinally/src/main/tutorial/Main.java
+++ ExceptionTryCatchdanFinally/src/main/tutorial/Main.java
@@ -1,5 +1,10 @@
 package main.tutorial;
-import java.util.*;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,25 +19,51 @@ public class Main {
 		
 		//System.out.println(nilai[3]);
 Scanner input = new Scanner(System.in);
-
+//runtime error jika file tidak ada
+FileInputStream fileInput = null; //tipe data fileInputStream namanya fileinput dengan isian masih kosong
 System.out.print("nilai Arrayke : ");
 
 int index = input.nextInt();
 
+//exception handling (try, catch, finally)
+System.out.println("Handling out of bound");
 try {
 //baris yg memunculkan exception, harus dibungkus dengan try karena kasus ini input index dari user melebihi index yg ada pada array
 	System.out.printf("index array ke %d, bernilai : %d",index, nilaiArray[index]);
 } 
 	catch (Exception pesanException) {
 //	atau bisa juga dengan penulisan 
-	//catch (java.lang.ArrayIndexOutOfBoundsException pesanException){
+	//catch (java.lang.ArrayIndexOutOfBoundsException pesanException){  ---->> sama saja dengan penulisan Exception ini adalah subException
 	System.out.println(pesanException);
 }
+
+//runtime error jika file tidak ada
+System.out.println("Handling IO not found");
+try {
+	fileInput = new FileInputStream("input.exe");
+	
+}catch (IOException e) {
+	System.err.println(e);
+}
+
+//menggabungkan dua exception
+System.out.println("Menggabungkan dua buah Exception");
+try {
+	System.out.printf("index ke-%d, adalah %d \n", index, nilaiArray[index]);
+	 fileInput = new FileInputStream("input.exe");
+}catch (ArrayIndexOutOfBoundsException pesanException) {
+	System.err.println("index melebihi jumlah array");
+}catch (IOException e) {System.err.println("file tdk ada");}
+
 //jika ada exception kita ambil maka diberi nama catch
 //catch apapun yg terjadi, yg terjadi adalah ada exception
 
 System.out.printf("akhir program,");
 //jika  error exception keluar maka program dibawah tidak dieksekusi
+
+
+
+
 	}
 	
 }

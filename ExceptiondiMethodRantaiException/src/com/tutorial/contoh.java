b2eac3dae11fdd98a78254f94c185b7f91e9b4e2
rio <riovectored@gmail.com>
2023-06-03 14:05:08 +0700

    menambahkan pengenalan IO stream input file

--- ExceptiondiMethodRantaiException/src/com/tutorial/contoh.java
+++ ExceptiondiMethodRantaiException/src/com/tutorial/contoh.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 
 public class contoh {
-		
+		//menaruh throws exception di main method, menjadi error message sebelum compiling
 		public static void main(String[] args) throws IOException {
 				FileInputStream inputFile = new FileInputStream("input.txt");
 			System.out.println((char)inputFile.read());
---
+++ Pengenalan_IO_Stream_Input_file/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author amanah
+ *
+ */
+module Pengenalan_IO_Stream_Input_file {
+}

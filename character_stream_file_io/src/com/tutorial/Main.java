651646b3e145ec6d3041710691f9660568003e16
rio <riovectored@gmail.com>
2023-06-17 16:06:56 +0700

    character stream file i o

--- character_stream_file_io/src/com/tutorial/Main.java
+++ character_stream_file_io/src/com/tutorial/Main.java
@@ -1,8 +1,10 @@
 package com.tutorial;
 
+import java.io.*;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		//sebelumnya menggunakan byte stream dengan 8 bit nya
 		//negar barat kebanyanyakn menggunakan 8 bit dengan ascii codennya
@@ -16,10 +18,38 @@ public class Main {
 		//membuka file
 //		byte stream selalu menggunakan FileInputStream
 //		Character stream menggunakan FileReader
+		FileInputStream byteFileInput = new FileInputStream("input.txt");
+		FileReader charFileInput = new FileReader("input.txt");
 		
+		FileOutputStream byteFileOutput = new FileOutputStream("outputByte.txt");
+		FileWriter charFileOutput = new FileWriter("outputChar.txt");
 		
 //		membaca file
+//		byte file
+		int buffer = byteFileInput.read();
+		
+		while(buffer != -1) {
+			System.out.print((char)buffer);
+			byteFileOutput.write(buffer);
+			buffer = byteFileInput.read();
+		}
+		
+		System.out.println("\n");
+//		char file
+		buffer = charFileInput.read();
+		
+		while (buffer != -1) {
+			System.out.print((char)buffer);
+			charFileOutput.write(buffer);
+			buffer = charFileInput.read();
+		}
+		
 //		menutup file
+	byteFileInput.close();
+	charFileInput.close();
+	byteFileOutput.close();
+	charFileOutput.close();
+	
 	}
 
 }
